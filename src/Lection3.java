60514e1b351c53b2317ffaa3440d0c684d8fb23a
YuriyKlots <sprklyots@gmail.com>
2019-05-07 19:08:03 +0300

    4 lection3

---
+++ src/Lection3.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.random;
+
+public class Lection3 {
+    Rectangle rect = new Rectangle(5,10);
+
+    double x = random()*10;
+
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle {
+    int width;
+    int height;
+    int squere;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+        squere = this.width * this.height;
+    }
+}
